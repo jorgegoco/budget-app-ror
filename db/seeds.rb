3bb936e907e7105e06056f7a1e92dab9a55a0ecb
jorgegoco <jorgegoco70@gmail.com>
2023-03-28 13:51:22 +0200

    Add Group views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+include Devise::Controllers::Helpers
+
+user1 = User.new({ name: 'Pepe', email: 'pepe@gmail.com', password: '123456', password_confirmation: '123456' })
+user2 = User.new({ name: 'Manolo', email: 'manolo@gmail.com', password: '654321', password_confirmation: '654321' })
+
+confirmation_token = Devise.token_generator.generate(User, :confirmation_token)
+user1.confirmation_token = confirmation_token[0]
+user1.confirmed_at = Time.now.utc
+
+if user1.save!
+  puts 'First user created successfully...'
+end
+
+confirmation_token = Devise.token_generator.generate(User, :confirmation_token)
+user2.confirmation_token = confirmation_token[0]
+user2.confirmed_at = Time.now.utc
+
+if user2.save!
+  puts 'Second user created successfully...'
+end
+
+group1 = Group.create({name: 'Sport', icon: 'https://cdn-icons-png.flaticon.com/512/2112/2112215.png', user_id: 1})
+group2 = Group.create({name: 'Gaming', icon: 'https://cdn-icons-png.flaticon.com/512/3588/3588658.png', user_id: 1})
+
+Operation.create([
+    {name: 'Tennis raquet', amount: 325, user_id: 1},
+    {name: 'Baseball Bat', amount: 135, user_id: 1}
+])
