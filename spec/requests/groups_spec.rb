7ddddf3a27b58471c05d9113c494acf1485d29f6
jorgegoco <jorgegoco70@gmail.com>
2023-05-28 20:22:18 +0200

    Add request rspec for groups

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,38 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe '/groups', type: :request do
-  let(:user) { User.create!(name: 'Another_user', email: 'test2@gmail.com', password: '123123') }
-
-  let(:valid_attributes) do
-    { user:, name: 'Shopping', icon: 'Icon_here' }
-  end
-
-  let(:invalid_attributes) do
-    { user:, name: 'Shopping', icon: '' }
+RSpec.describe 'Groups', type: :request do
+  include Devise::Test::IntegrationHelpers
+  before do
+    @user = User.create(name: 'pepe', email: 'pepe@gmail.com', password: '123123')
+    @group = @user.groups.create(name: 'Some', icon: 'some')
+    sign_in @user
   end
 
   describe 'GET /index' do
-    it 'renders a successful response' do
-      Group.create! valid_attributes
-      get groups_url
-      expect(response).to be_successful
+    it 'returns http success' do
+      get groups_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('Some')
     end
   end
 
-  describe 'GET /new' do
-    it 'renders a successful response' do
-      get new_group_url
-      expect(response).to be_successful
-    end
-  end
+# RSpec.describe GroupsController, type: :request do
+#   let(:user) { User.create(name: 'Pepe', email: 'pepe@gmail.com', password: '123123') }
 
-  describe 'POST /create' do
-    context 'with valid parameters' do
-      it 'creates a new Group' do
-        expect do
-          Group.create! valid_attributes
-        end.to change(Group, :count).by(1)
-      end
-    end
-  end
+#   describe 'GET #index' do
+#     it 'returns a success response' do
+#       Group.create!(name: 'Test Group', icon: 'icon', user: user)
+#       get groups_url
+#       # puts response.body
+#       # expect(response.body).to include('Test Group')
+#       expect(response).to have_http_status(:ok)
+#       expect(response).to render_template('index')
+#     end
+#   end
+
+  # describe 'GET /new' do
+  #   it 'renders a successful response' do
+  #     get new_group_url
+  #     expect(response).to be_successful
+  #   end
+  # end
+
+  # describe 'POST /create' do
+  #   context 'with valid parameters' do
+  #     it 'creates a new Group' do
+  #       expect do
+  #         Group.create! valid_attributes
+  #       end.to change(Group, :count).by(1)
+  #     end
+  #   end
+  # end
 end
